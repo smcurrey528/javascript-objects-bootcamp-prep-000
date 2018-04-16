06d8ad0cd9f2715aafd58858df557023820a7128
Sarah Currey <curreysarah28@gmail.com>
2018-04-16 01:57:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,4 +2,6 @@ var playlist = {
   Alison: "Slow Dive",
   MyBloodyValentine: "Sometimes"
 };
-function updatePlaylist ()
+function updatePlaylist (playlist, 'Phil Ochs', "Here's to the State of Mississippi") {
+  return Object.assign ({}, playlist, {['Phil Ochs']:"Here;s to the State of Mississippi"})
+}
