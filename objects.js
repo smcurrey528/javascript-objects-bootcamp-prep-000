c0ffb6179b5866203e6464795f368b78b1e6eecc
Sarah Currey <curreysarah28@gmail.com>
2018-04-16 02:07:03 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,6 +4,6 @@ var playlist = {
 };
 
 function updatePlaylist (playlist,'Phil Ochs', 'Heres to the State of Mississippi') {
-  return Object.assign ({}, playlist, {['Phil Ochs']:"Here;s to the State of Mississippi"})
+  return Object.assign ({}, playlist, {['Phil Ochs']:"Heres to the State of Mississippi"})
 }
 function removeFromPlaylist (playlist)
